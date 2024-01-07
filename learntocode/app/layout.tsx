1dd5855f08e69a8b79a6e0e174e2179c05b7353d
Zak Thompson <zakthompson97@gmail.com>
2024-01-07 17:13:55 -0600

    Initial commit

---
+++ learntocode/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Learn To Code',
+  description: 'An AI-powered app intended to help beginners take their first step into programming',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
