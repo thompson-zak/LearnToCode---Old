9ae5df3d5221fa811e0de803e9dc6dff217233ac
Zak Thompson <zakthompson97@gmail.com>
2024-01-07 18:42:27 -0600

    Fix tutorials layout repeating issue. Update homepage hrefs.

--- learntocode/app/page.tsx
+++ learntocode/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
         </Link>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="/tutorials/data"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -72,10 +72,8 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/tutorials/conditionals"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Conditionals{' '}
@@ -89,10 +87,8 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/tutorials/oop"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             OOP{' '}
--- learntocode/app/tutorials/layout.tsx
+++ learntocode/app/tutorials/layout.tsx
@@ -3,10 +3,12 @@ export default function TutorialLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <div>
-        <p>This is a custom layout for tutorials only</p>
-        {children}
-    </div>
+  return (   
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+            <p>This is a custom layout for tutorials only</p>
+            {children}
+        </div>
+    </main>
   )
 }
