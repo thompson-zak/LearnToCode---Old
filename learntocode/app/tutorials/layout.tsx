2e69a588c608bd3abb653b64d8fb8511e5f08e24
Zak Thompson <zakthompson97@gmail.com>
2024-01-08 21:04:29 -0600

    Add home button in tutorials page

--- learntocode/app/tutorials/layout.tsx
+++ learntocode/app/tutorials/layout.tsx
@@ -8,6 +8,22 @@ export default function TutorialLayout({
         <div className="z-10 max-w-6xl w-full items-center justify-between font-mono text-sm lg:flex">
             {children}
         </div>
+        <div className="max-w-6xl w-full">
+          <a
+            href="/"
+            className="float-left group rounded-lg border border-transparent px-4 py-3 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              <span className="inline-block transition-transform group-hover:-translate-x-1 motion-reduce:transform-none">
+                &lt;-
+              </span>
+              Home{' '}
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Go back to start another lesson.
+            </p>
+          </a>
+        </div>
     </main>
   )
 }
